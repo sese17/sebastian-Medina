eaf355fc4085d24e7ebde0b3cc61fd90ca605227
sese17 <106594793+sese17@users.noreply.github.com>
2024-04-12 18:53:58 -0300

    Update server.py

--- chat (socket)/server.py
+++ chat (socket)/server.py
@@ -1,23 +1,23 @@
 import socket
 
 def main():
-    # Configuración del servidor
+    
     host = '127.0.0.1'
     port = 8080
 
-    # Creación del socket del servidor
+    
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(1)  # Solo se permite una conexión
 
     print("Servidor escuchando en {}:{}".format(host, port))
 
-    # Espera a que un cliente se conecte
+    
     client_socket, addr = server_socket.accept()
     print("Cliente conectado desde:", addr)
 
     while True:
-        # Espera a recibir datos del cliente
+        
         data = client_socket.recv(1024).decode("utf-8")
         if not data:
             break
